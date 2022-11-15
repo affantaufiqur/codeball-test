337a7ff003ba1e1dc92794585be8afccb64ee690
Affan <affantaufiqur@gmail.com>
2022-11-15 23:56:07 +0700

    fix import component that dont exist

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ function App() {
   return (
     <div className="container mx-auto">
       <div className="text-center font-semibold text-sm">react + typescript + tailwind</div>
-      <SomeUnknowComponent />
     </div>
   )
 }
